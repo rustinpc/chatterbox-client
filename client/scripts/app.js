7ea8771febc8ff860047ba3a797c17bca7e5dcd2
rustinpc <rustinpc@gmail.com>
2014-11-11 11:09:04 -0800

    Users can add friends and messages from friends will be bolded.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,29 +1,17 @@
 // YOUR CODE HERE:
+var friendList = {};
+var addFriend = function() {
+  var value = $(this)[0].innerText;
+  var lowerVal = value.toLowerCase();
+  if (!friendList[lowerVal]) {
+    friendList[lowerVal] = value;
+  }
+}
+
 $(document).ready(function () {
 
   var currentRoom = 'lobby';
   var roomList = {};
-
-  var getFunc = function() {
-
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      data: {limit: 100, order: "-createdAt"},
-      contentType: 'application/json',
-      success: function (data) {
-        messageWriter(data.results);
-      },
-      error: function () {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message');
-      }
-    });
-  };
-
-  getFunc();
-  setInterval(getFunc, 1000);
-
 
   var submitFunc = function() {
     var value = $(".userInput").val();
@@ -45,6 +33,9 @@ $(document).ready(function () {
     });
   };
 
+ var befriend = function() {
+    console.log(44);
+  };
 
   var makeNewRoom = function() {
     var value = $(".roomInput").val();
@@ -67,10 +58,13 @@ $(document).ready(function () {
   };
 
 
+
+
   //room id
   $("#main").append("<select class='roomList'><option value='lobby'>Lobby</option></select><br><input type='button' class='createRoom'><input type='text' class='roomInput'><br><br>");
 
   $(".roomList").on('change',changeRooms);
+
 
   $(".createRoom").on('click',makeNewRoom);
 
@@ -83,15 +77,19 @@ $(document).ready(function () {
   // print out chats
   $("#main").append("<ul class='messageList'></ul>");
 
-
+// $(this).parent().addClass('friend');
 
   var messageWriter = function(data) {
 
     var chatter = '';
     for (var i = 0; i < data.length; i++) {
       if (data[i].roomname === currentRoom) {
-
-        chatter += "<li>" + encodeURIComponent(data[i].username) + ": " + encodeURIComponent(data[i].text) + "</li>";
+        var friendLower = data[i].username;
+        if (friendList[friendLower.toLowerCase()]) {
+          chatter += "<li class='friend'><span class='random' onclick='addFriend.call(this)' >" + encodeURIComponent(data[i].username) + "</span>: " + encodeURIComponent(data[i].text) + "</li>";
+        } else {
+          chatter += "<li><span class='random' onclick='addFriend.call(this)' >" + encodeURIComponent(data[i].username) + "</span>: " + encodeURIComponent(data[i].text) + "</li>";
+        }
       }
     }
     $('.messageList').html('');
@@ -100,5 +98,23 @@ $(document).ready(function () {
     console.log();
   };
 
+  var getFunc = function() {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: {limit: 100, order: "-createdAt"},
+      contentType: 'application/json',
+      success: function (data) {
+        messageWriter(data.results);
+      },
+      error: function () {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
+
+  getFunc();
+  setInterval(getFunc, 1000);
 });
 
